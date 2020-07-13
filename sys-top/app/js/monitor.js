68ca7c8a490e2a203b4117bb1add5e0f3a2d308b
shafirpl <shafi.rpl@gmail.com>
2020-07-13 00:24:49 -0700

    Added memory monitoring stuff

--- sys-top/app/js/monitor.js
+++ sys-top/app/js/monitor.js
@@ -1,104 +1,135 @@
-const path = require('path')
-const osu = require('node-os-utils')
-const { Notification } = require('electron')
-const cpu = osu.cpu
-const mem = osu.mem
-const os = osu.os
-const notifier = require('node-notifier');
+const path = require("path");
+const osu = require("node-os-utils");
+const { Notification, ipcRenderer } = require("electron");
 
-let cpuOverload = 10
-let alertFreq = 1
+const cpu = osu.cpu;
+const mem = osu.mem;
+const os = osu.os;
+const notifier = require("node-notifier");
+
+const si = require("systeminformation");
+
+let cpuOverload;
+let alertFreq;
+let physicalCpuCount;
+
+let setCpuModel = async () => {
+  try {
+    const data = await si.cpu();
+    physicalCpuCount = data.physicalCores;
+    // set cpu model
+    document.getElementById(
+      "cpu-model"
+    ).innerText = `${cpu.model()}\n ${physicalCpuCount} Cores/ ${cpu.count()} Threads `;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// get settings and values
+ipcRenderer.on("settings:get", (e, settings) => {
+  // + sign converts it to a number, but we could also use parseInt or Number function for that,
+  // don't know why brad uses this
+  cpuOverload = +settings.cpuOverload;
+  alertFreq = +settings.alertFrequency;
+});
 
 // send notification
 const notifyUser = (options) => {
-    notifier.notify(options)
-}
+  notifier.notify(options);
+};
 
 // check how much time has passed since notification
 const runNotify = (freq) => {
-    if (localStorage.getItem('lastNotify') == null){
-        // store timestamp
-        localStorage.setItem('lastNotify', +new Date())
-        return true
-    }
-    const notifyTime = new Date(parseInt(localStorage.getItem('lastNotify')))
-    const now = new Date()
-    const diffTime = Math.abs( now - notifyTime)
-    const minutesPassed = Math.ceil(diffTime/ (60*1000))
+  if (localStorage.getItem("lastNotify") == null) {
+    // store timestamp
+    localStorage.setItem("lastNotify", +new Date());
+    return true;
+  }
+  const notifyTime = new Date(parseInt(localStorage.getItem("lastNotify")));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.ceil(diffTime / (60 * 1000));
 
-    if (minutesPassed>freq){
-        return true
-    }
-    return false
+  if (minutesPassed > freq) {
+    return true;
+  }
+  return false;
+};
 
-}
-
+setCpuModel();
 
 /*
-* for other info, we need to update it at a set interval, so we need to run it every 2 second 
-* for which we need to use setInterval function
-*/
+ * for other info, we need to update it at a set interval, so we need to run it every 2 second
+ * for which we need to use setInterval function
+ */
 setInterval(() => {
-    // toFixed method: https://www.w3schools.com/JSREF/jsref_tofixed.asp
-    
+  // toFixed method: https://www.w3schools.com/JSREF/jsref_tofixed.asp
+   si.mem().then((data) => 
+   {console.log(data.free/(1000*1000*100))});
 
-    // cpu usage
-    cpu.usage().then(info => {
-        document.getElementById('cpu-usage').innerText = `${info.toFixed(2)} %`
-        // sets the progress bar
-        document.querySelector('#cpu-progress').style.width = `${info}%`
+    // mem usage
+    mem.info().then((info) => {
+      document.getElementById("mem-usage").innerText = `${
+        (100 - info.freeMemPercentage).toFixed(2)
+      }% | ${(info.usedMemMb/1024).toFixed(2)}GB`;
+      document.getElementById('mem-free').innerText = `${(info.freeMemPercentage).toFixed(2)}% | ${(info.freeMemMb/1000).toFixed(2)}GB`
+    });
 
-        // make progress bar red if overload
-        if (info > cpuOverload){
-            document.querySelector('#cpu-progress').style.background = `red`
-        } else{
-            document.querySelector('#cpu-progress').style.background = `#30c88b`
-        }
+  // cpu usage
+  cpu.usage().then((info) => {
+    document.getElementById("cpu-usage").innerText = `${info.toFixed(2)} %`;
+    // sets the progress bar
+    document.querySelector("#cpu-progress").style.width = `${info}%`;
 
-        if(info > cpuOverload && runNotify(alertFreq)){
-            // notify users
-            notifyUser({
-                title: 'CPU Overload',
-                message: `CPU is over ${cpuOverload}%`,
-                icon: path.join(__dirname, 'img', 'icon.png')
-            })
-            localStorage.setItem('lastNotify', +new Date())
-        }
+    // make progress bar red if overload
+    if (info > cpuOverload) {
+      document.querySelector("#cpu-progress").style.background = `red`;
+    } else {
+      document.querySelector("#cpu-progress").style.background = `#30c88b`;
+    }
 
-    })
-    // cpu free
-    cpu.free().then(info => {
-        document.getElementById('cpu-free').innerText = `${info.toFixed(2)} %`
-    })
-    // uptime
-    document.getElementById('sys-uptime').innerText = convertSecondsToDHMS(os.uptime())
-},2000)
-
-// set cpu model
-document.getElementById('cpu-model').innerText = cpu.model()
+    if (info > cpuOverload && runNotify(alertFreq)) {
+      // notify users
+      notifyUser({
+        title: "CPU Overload",
+        message: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, "img", "icon.png"),
+      });
+      localStorage.setItem("lastNotify", +new Date());
+    }
+  });
+  // cpu free
+  cpu.free().then((info) => {
+    document.getElementById("cpu-free").innerText = `${info.toFixed(2)} %`;
+  });
+  // uptime
+  document.getElementById("sys-uptime").innerText = convertSecondsToDHMS(
+    os.uptime()
+  );
+}, 2000);
 
 //computer name
-document.getElementById('comp-name').innerText = os.hostname()
+document.getElementById("comp-name").innerText = os.hostname();
 
 // os
-document.getElementById('os').innerText = `${os.type()} ${os.arch()}`
+document.getElementById("os").innerText = `${os.type()} ${os.arch()}`;
 
 // total memory
 // it returns a promise for which we need to extract the info
-mem.info().then(info => {
-    // even though it is in mb, it actually gives the info in total gigabyte, which is weird
-    document.getElementById('mem-total').innerText = `${info.totalMemMb} GB`
-})
+mem.info().then((info) => {
+  const totalMem = info.totalMemMb / 1024
+  totalMem.toFixed(2)
+  document.getElementById("mem-total").innerText = `${totalMem}GB`;
+});
 
 // show days, minutes, seconds
 const convertSecondsToDHMS = (seconds) => {
-    // converting to number, we could use parseInt, don't know why brad uses this weird stuff
-    seconds += seconds
-    const days = Math.floor(seconds / (3600*24))
-    const hours = Math.floor((seconds % (3600*24))/ 3600 )
-    const minutes = Math.floor((seconds % 3600) / 60)
-    const secondsUptime = Math.floor(seconds % 60)
-    return `${days}D,${hours}H,${minutes}M,${secondsUptime}S`
-}
-
-
+  // converting to number, we could use parseInt, don't know why brad uses this weird stuff
+  seconds += seconds;
+  const days = Math.floor(seconds / (3600 * 24));
+  const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secondsUptime = Math.floor(seconds % 60);
+  return `${days}D,${hours}H,${minutes}M,${secondsUptime}S`;
+};
--- sys-top/main.js
+++ sys-top/main.js
@@ -1,13 +1,30 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain, Tray } = require('electron')
 const log = require('electron-log')
+const Store = require('./Store')
+const path = require('path')
 
 // Set env
 process.env.NODE_ENV = 'development'
+// process.env.NODE_ENV = "production";
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
 let mainWindow
+let tray
+
+
+
+// initializing store and defaults
+const store = new Store({
+  configName: 'user-settings',
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5
+    }
+  }
+})
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -32,9 +49,58 @@ function createMainWindow() {
 
 app.on('ready', () => {
   createMainWindow()
+  mainWindow.webContents.on('dom-ready', () => {
+    mainWindow.webContents.send('settings:get', store.get('settings'))
+  })
+
+  
 
   const mainMenu = Menu.buildFromTemplate(menu)
   Menu.setApplicationMenu(mainMenu)
+
+  /*
+  * When we close the window using the x button on Mac, we just want to hide it instead of quiting it, and quit it 
+  * either from clicking quit from the tray icon or from going to file menu and quit. This code ensures that
+  */
+  mainWindow.on('close',e => {
+    if (!app.isQuiting){
+      e.preventDefault()
+      mainWindow.hide()
+    }
+
+    return true
+  })
+
+  const icon = path.join(__dirname,'assets', 'icons', 'tray_icon.png')
+
+  // creating tray
+  tray = new Tray(icon)
+
+  // this hides and shows the window when we click on the tray icon
+  tray.on('click', () => {
+    if (mainWindow.isVisible() === true){
+      mainWindow.hide()
+    }
+    else{
+      mainWindow.show()
+    }
+  })
+
+  // when we right click on the tray icon, we will see a menu with quit, this code
+  // allows that functionality
+  tray.on('right-click', () => {
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Quit',
+        click: () => {
+          app.isQuiting = true
+          app.quit()
+        }
+      }
+    ])
+    tray.popUpContextMenu(contextMenu)
+  })
+
 })
 
 const menu = [
@@ -56,6 +122,12 @@ const menu = [
       ]
     : []),
 ]
+
+// Set settings
+ipcMain.on('settings:set', (e, value) => {
+  store.set('settings', value)
+   mainWindow.webContents.send("settings:get", store.get("settings"));
+})
 
 app.on('window-all-closed', () => {
   if (!isMac) {
